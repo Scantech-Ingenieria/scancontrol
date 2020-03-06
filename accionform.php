91edc356259c06b0c29a563143a461fddc21db41
FelipeBenavidesSc <61832244+FelipeBenavidesSc@users.noreply.github.com>
2020-03-06 11:12:44 -0300

    Add files via upload

---
+++ accionform.php
@@ -0,0 +1,33 @@
+<?php 
+
+require_once "model.php";
+
+
+
+ if (isset($_POST['user'])) {
+			$tabla = "admin";
+			$usuario = $_POST["user"];
+
+			$respuesta = ModeloSesion::iniciarSesionMdl($tabla, $usuario);
+			
+			if($respuesta["nombre"] == $_POST["user"] && $respuesta["clave"] == $_POST["password"]) {
+				    session_start();
+
+				$_SESSION["autenticar"] = "ok";
+				$_SESSION["nombre"] = $respuesta["nombre"];
+			
+
+
+
+				echo 'exito';
+
+			} else {
+				echo '	<div class="alert alert-danger">
+						Datos incorrectos. Inténtelo nuevamente.
+					</div>	';
+			}
+		}
+
+
+
+?>
